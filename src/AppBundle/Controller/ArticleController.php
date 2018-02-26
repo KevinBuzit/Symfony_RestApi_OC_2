51e35de79ba9e98ae3de2c21792930888977c4c1
KevinBuzit <buzitk@gmail.com>
2018-02-26 13:57:09 +0100

    Deserialize

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -2,19 +2,23 @@
 
 namespace AppBundle\Controller;
 
-use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use AppBundle\Entity\Article;
-use FOS\RestBundle\Controller\Annotations\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class ArticleController
+class ArticleController extends Controller
 {
     /**
-     * @Get(
+     * @Rest\Get(
      *     path = "/articles/{id}",
      *     name = "app_article_show",
      *     requirements = {"id"="\d+"}
      * )
-     * @View(
+     * @Rest\View(
      *     statusCode = 200,
      *     serializerGroups = {"POST_CREATE"}
      * )
@@ -22,5 +26,33 @@ class ArticleController
     public function showAction(Article $article)
     {
         return $article;
+    }
+
+    /**
+     * @Rest\Post(
+     *    path = "/articles",
+     *    name = "app_article_create"
+     * )
+     * @Rest\View(StatusCode = 201)
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+    public function createAction(Article $article)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($article);
+        $em->flush();
+
+        return $article;
+    }
+
+    /**
+     * @Rest\Get("/articles", name="app_article_list")
+     */
+    public function listAction()
+    {
+        $articles = $this->getDoctrine()->getRepository('AppBundle:Article')->findAll();
+        
+        return $articles;
     }
 }
